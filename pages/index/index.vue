bbe20e6f0b627548599f7b5a962a9ed0bb882226
qfrx <1817721918@qq.com>
2023-07-18 19:36:21 +0800

    first commit

---
+++ pages/index/index.vue
@@ -0,0 +1,42 @@
+<template>
+	<view class="content">
+		<image class="logo" src="/static/logo.png"></image>
+		<view class="text-area">
+			<text class="title">{{title}}</text>
+		</view>
+
+    <view class="">
+		<u-search placeholder="日照香炉生紫烟" v-model="keyword"></u-search>
+    </view>
+	<u-button type="success" :plain="false" :hairline="true" text="细边"></u-button>
+	</view>
+</template>
+<!-- <template>
+	
+</template> -->
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello',
+				keyword: '遥看瀑布挂前川'
+			}
+		},
+		onLoad() {
+			console.log(uni.$u.config.v);
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss">
+  .text-area{
+	  .title{
+		  color: red;
+	  }
+  }
+</style>
